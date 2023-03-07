065843e3525ddf6d0ed26fa110b8a5fda62ec7d0
Ibrahim <imimazim7@gmail.com>
2023-03-08 01:12:52 +0200

    feat: added helper functions

---
+++ helperfunctions.cpp
@@ -0,0 +1,41 @@
+#include "helperfunctions.h"
+
+HelperFunctions::HelperFunctions()
+{
+
+}
+
+HelperFunctions::~HelperFunctions()
+{
+
+}
+
+QPixmap HelperFunctions::convertQByteArrToPixmap(QByteArray arr){
+    QPixmap pixmap;
+    pixmap.loadFromData(arr);
+    return pixmap;
+}
+
+QPixmap HelperFunctions::convertMatToPixmap(Mat imageMat){
+    Mat rgbImage;
+    cvtColor(imageMat, rgbImage, COLOR_BGR2RGB);
+    QImage qimage(rgbImage.data, rgbImage.cols, rgbImage.rows, static_cast<int>(rgbImage.step), QImage::Format_RGB888);
+    QPixmap pixmap= QPixmap::fromImage(qimage);
+    return pixmap;
+}
+
+QByteArray HelperFunctions::readImage_QByte(){
+    QString filePath = QFileDialog::getOpenFileName(nullptr, "Open Image File", "", "Image Files (*.png *.jpg *.bmp)");
+
+    QFile file(filePath);
+    file.open(QIODevice::ReadOnly);
+    QByteArray imageData = file.readAll();
+
+    return imageData;
+}
+
+Mat HelperFunctions::readImage_Mat(){
+  QString filePath = QFileDialog::getOpenFileName(nullptr, "Open Image File", "", "Image Files (*.png *.jpg *.bmp)");
+  cv::Mat image = cv::imread(filePath.toStdString());
+  return image;
+}
---
+++ helperfunctions.h
@@ -0,0 +1,24 @@
+#ifndef HELPERFUNCTIONS_H
+#define HELPERFUNCTIONS_H
+
+#include <QByteArray>
+#include <QPixmap>
+#include <QFileDialog>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+
+class HelperFunctions
+{
+public:
+  HelperFunctions();
+  ~HelperFunctions();
+  static QByteArray readImage_QByte();
+  static Mat readImage_Mat();
+  static QPixmap convertQByteArrToPixmap(QByteArray arr);
+  static QPixmap convertMatToPixmap(Mat imageMat);
+};
+
+#endif // HELPERFUNCTIONS_H
