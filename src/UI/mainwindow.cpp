9f2dfc0e4b9aaaea0e4079e8a8deb039ed6e9221
Ibrahim <imimazim7@gmail.com>
2023-03-24 23:33:46 +0200

    feat: edited class mainWindow to be compatible with the expansion

--- src/UI/mainwindow.cpp
+++ src/UI/mainwindow.cpp
@@ -1,45 +1,31 @@
-/******************************************************************************
- *
- * File Name: mainwindow.cpp
- * Description: Source file for the GUI main window of the program
- * Authors: Ibrahim Mohamed
- * Last Modified: 3 Mar 23 - 01:40
- *
- *******************************************************************************/
-
-#include <QMessageBox>
-#include <QDebug>
-
 #include "mainwindow.h"
+#include "filterswidget.h"
+#include "mixingwidget.h"
+#include "histogramswidget.h"
+#include "houghwidget.h"
+
 #include "ui_mainwindow.h"
 
-#include <QtCharts/QChartView>
-#include <QtCharts/QBarSeries>
-
-#include "src/Helpers/helperfunctions.h"
-#include "src/Frequency/fouriermixer.h"
-
-#include "src/Filters/imagesmoothers.h"
-#include "src/Filters/edgedetectors.h"
-#include "src/Filters/noiseaddatives.h"
-#include "src/Histograms/histograms.h"
-
-
-using namespace cv;
-
-Image filterTabImage = {},
-grayscaleTabImage = {},
-HistoTabImage = {},
-HybridImage1 = {},
-HybridImage2 = {};
-
-float d1note = 30.0,
-d2note = 30.0;
-MainWindow::MainWindow(QWidget *parent) :
-  QMainWindow(parent),
-  ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent)
+  : QMainWindow(parent)
+  , ui(new Ui::MainWindow)
 {
   ui->setupUi(this);
+
+  index = 0;
+
+  FiltersWidget *myFilterWidget = new FiltersWidget();
+  MixingWidget *myMixingWidget = new MixingWidget();
+  HistogramsWidget *myHistogramWidget = new HistogramsWidget();
+  HoughWidget * muHoughWidget = new HoughWidget();
+
+
+  ui->stackedWidget->addWidget(myFilterWidget);
+  ui->stackedWidget->addWidget(myMixingWidget);
+  ui->stackedWidget->addWidget(myHistogramWidget);
+  ui->stackedWidget->addWidget(muHoughWidget);
+
+  ui->stackedWidget->setCurrentIndex(index);
 }
 
 MainWindow::~MainWindow()
@@ -47,368 +33,19 @@ MainWindow::~MainWindow()
   delete ui;
 }
 
-void MainWindow::updateFilteredPicture(Mat updatedImage){
-
-  qDebug() << updatedImage.type();
-  filterTabImage.setFilteredImage(updatedImage);
-  QPixmap pixmap = HelperFunctions::convertMatToPixmap(updatedImage);
-  QPixmap scaledpixmap = pixmap.scaled(ui->tabFilterFilteredImage->size(), Qt::IgnoreAspectRatio);
-  ui->tabFilterFilteredImage->setPixmap(scaledpixmap);
-}
-
-void MainWindow::updateCurrentPicture(Mat updatedImage){
-  filterTabImage.setCurrentImage(updatedImage);
-  QPixmap pixmap = HelperFunctions::convertMatToPixmap(updatedImage);
-  QPixmap scaledpixmap = pixmap.scaled(ui->tabFilterFilteredImage->size(), Qt::IgnoreAspectRatio);
-  ui->tabFilterOriginalImage->setPixmap(scaledpixmap);
-}
-
-void MainWindow::downloadImage(Mat image){
-  QString fileName = QFileDialog::getSaveFileName(nullptr, "Save Image", QDir::homePath(), "PNG Image (*.png);;JPEG Image (*.jpg)");
-
-  if (!fileName.isEmpty()) {
-
-
-      bool result = cv::imwrite(fileName.toStdString(), image);
-
-      if (!result) {
-          QMessageBox::warning(nullptr, "Error", "Failed to save image!");
-        }
-    }
-}
-
-void MainWindow::plotHistogram(const vector<int>& data , QString ChartName, QChartView* widget){
-  QChart *chart = new QChart();
-  QBarSeries *series = new QBarSeries();
-
-  QBarSet *set = new QBarSet("Data");
-  for (int value : data) {
-      *set << value;
-    }
-
-  series->append(set);
-  chart->addSeries(series);
-
-
-  chart->setTitle(ChartName);
-  chart->setBackgroundBrush(Qt::black);
-  chart->layout()->setContentsMargins(0, 0, 0, 0);
-  widget->setChart(chart);
-}
-
-
-// ------------------------  Filter Tab Handlers ------------------------ //
-
-// upload & download
-void MainWindow::on_uploadBtn_clicked()
-{
-  Mat Image = HelperFunctions::readImage_Mat();
-  if (Image.cols != 1 && Image.rows != 1){
-      filterTabImage.setFirstUpload(Image);
-      QPixmap pixmap = HelperFunctions::convertMatToPixmap(Image);
-      QPixmap scaledpixmap = pixmap.scaled(ui->tabFilterOriginalImage->size(), Qt::IgnoreAspectRatio);
-      ui->tabFilterOriginalImage->setPixmap(scaledpixmap);
-    }
-}
-
-void MainWindow::on_downloadBtn_clicked()
-{
-  downloadImage(filterTabImage.getFilteredImage());
-}
-
-// Photo Smoothness
-void MainWindow::on_averageRemoveBtn_clicked()
-{
-  Mat outputImage = ImageSmoothers::applyAverageFilter(filterTabImage.getCurrentImage(), filterTabImage.kenerlSize);
-  updateFilteredPicture(outputImage);
-}
-
-void MainWindow::on_gaussianRemoveBtn_clicked()
-{
-  Mat outputImage = ImageSmoothers::applyGaussianFilter(filterTabImage.getCurrentImage(), filterTabImage.kenerlSize, filterTabImage.sigma);
-  updateFilteredPicture(outputImage);
-}
-
-void MainWindow::on_medianRemoveBtn_clicked()
-{
-
-  Mat outputImage = ImageSmoothers::applyMedianFilter(filterTabImage.getCurrentImage(), filterTabImage.kenerlSize);
-  updateFilteredPicture(outputImage);
-}
-
-
-// Noise addition
-void MainWindow::on_uniformNoiseBtn_clicked()
-{
-  // @TODO: HERE IMPLEMENT
-}
-
-void MainWindow::on_saltNoiseBtn_clicked()
-{
-  Mat outputImage = NoiseAddatives::SaltAndPepperNoise(filterTabImage.getCurrentImage());
-  updateFilteredPicture(outputImage);
-}
-
-void MainWindow::on_gaussianNoiseBtn_clicked()
-{
-  Mat outputImage = NoiseAddatives::GaussianNoise(filterTabImage.getCurrentImage(), filterTabImage.sigma , filterTabImage.mean);
-  updateFilteredPicture(outputImage);
-}
-
-// Edge Detectors
-void MainWindow::on_sobelBtn_clicked()
-{
-  Mat outputImage = EdgeDetectors::SobalEdgeDetector(filterTabImage.getCurrentImage());
-  updateFilteredPicture(outputImage);
-}
-
-void MainWindow::on_robertsBtn_clicked()
-{
-  Mat outputImage = EdgeDetectors::RobertEdgeDetector(filterTabImage.getCurrentImage());
-  updateFilteredPicture(outputImage);
-}
-
-void MainWindow::on_prewittBtn_clicked()
-{
-  Mat outputImage = EdgeDetectors::PerwittEdgeDetector(filterTabImage.getCurrentImage());
-  updateFilteredPicture(outputImage);
-}
-
-void MainWindow::on_cannyBtn_clicked()
-{
-  //  Mat outputImage = ImageSmoothers::cannyEdgeDetection(filterTabImage.getCurrentImage(), 50,150);
-  Mat outputImage = EdgeDetectors::CannyEdgeDetector(filterTabImage.getCurrentImage());
-
-  updateFilteredPicture(outputImage);
-}
-
-// Frequency Filters
-void MainWindow::on_lowFrequencyFilterBtn_clicked()
-{
-  Mat outputImage = FourierMix::apply_filter(filterTabImage.getCurrentImage(), "Ideal Low Pass", 30.0);
-  updateFilteredPicture(outputImage);
-}
-
-void MainWindow::on_highFrequencyFilterBtn_clicked()
-{
-  Mat outputImage = FourierMix::apply_filter(filterTabImage.getCurrentImage(), "Ideal High Pass", 30.0);
-  updateFilteredPicture(outputImage);
-}
-
-void MainWindow::on_gaussianFrequencyFilterBtn_clicked()
-{
-  Mat outputImage = FourierMix::apply_filter(filterTabImage.getCurrentImage(), "Gaussian", 30.0);
-  updateFilteredPicture(outputImage);
-}
-
-// Controls
-void MainWindow::on_applyBtn_clicked()
-{
-  updateCurrentPicture(filterTabImage.getFilteredImage());
-}
-
-void MainWindow::on_resetBtn_clicked()
-{
-  updateCurrentPicture(filterTabImage.getOriginalImage());
-}
-
-
-// ------------------------  Grayscale Tab Handlers ------------------------ //
-
-
-void MainWindow::on_uploadRGB_clicked()
-{
-  Mat Image = HelperFunctions::readImage_Mat();
-
-  if (Image.cols != 1 && Image.rows != 1){
-      grayscaleTabImage.setFirstUpload(Image);
-      QPixmap pixmap = HelperFunctions::convertMatToPixmap(Image);
-      QPixmap scaledpixmap = pixmap.scaled(ui->tabGrayOriginalImage->size(), Qt::IgnoreAspectRatio);
-      ui->tabGrayOriginalImage->setPixmap(scaledpixmap);
-
-      // convert to grayscale
-      Mat grayscaleMat;
-      cvtColor(Image,grayscaleMat, COLOR_BGR2GRAY);
-      grayscaleTabImage.setFilteredImage(grayscaleMat);
-
-      // view  the gray scale image
-      QPixmap grayPixmap = HelperFunctions::convertMatToPixmap(grayscaleMat);
-      QPixmap scaledgary = grayPixmap.scaled(ui->tabGrayGrayImage->size(), Qt::IgnoreAspectRatio);
-      ui->tabGrayGrayImage->setPixmap(scaledgary);
-
-      // create the histograms
-      Mat histogramCDF = Histograms::plotRGBHistogramCDF(Image);
-      Mat histogramPDF = Histograms::plotRGBHistogramPDF(Image);
-
-      // display the histograms
-      QPixmap hist1 = HelperFunctions::convertMatToPixmap(histogramCDF);
-      QPixmap scaledHist1 = hist1.scaled(ui->tabGrayHist1->size(), Qt::IgnoreAspectRatio);
-      ui->tabGrayHist1->setPixmap(scaledHist1);
-
-      QPixmap hist2 = HelperFunctions::convertMatToPixmap(histogramPDF);
-      QPixmap scaledHist2 = hist2.scaled(ui->tabGrayHist2->size(), Qt::IgnoreAspectRatio);
-      ui->tabGrayHist2->setPixmap(scaledHist2);
-    }
-}
-
-void MainWindow::on_downloadGray_clicked()
-{
-  downloadImage(grayscaleTabImage.getFilteredImage());
-}
-
-
-// ------------------------  Histograms Tab Handlers ------------------------ //
-
-void MainWindow::on_uploadHist_clicked()
-{
-  Mat Image = HelperFunctions::readImage_Mat();
-
-  if (Image.cols != 1 && Image.rows != 1){
-      HistoTabImage.setFirstUpload(Image);
-      Mat grayscaleMat;
-      cvtColor(Image,grayscaleMat, COLOR_BGR2GRAY);
-
-      QPixmap pixmap = HelperFunctions::convertMatToPixmap(grayscaleMat);
-
-      QPixmap scaledpixmap = pixmap.scaled(ui->tabHistOriginalImage->size(), Qt::IgnoreAspectRatio);
-      ui->tabHistOriginalImage->setPixmap(scaledpixmap);
-
-      // Equalize
-      vector<int> histogram = Histograms::Histo(grayscaleMat);
-      vector<int> cumlautiveHistogram = Histograms::cumHist(histogram);
-      vector<int> scale = Histograms::calcuateScale(Image, cumlautiveHistogram);
-
-      Mat matEqualized = Histograms::equilization(grayscaleMat, scale);
-      HistoTabImage.setFilteredImage(matEqualized);
-      QPixmap equalizedPixmap = HelperFunctions::convertMatToPixmap(matEqualized);
-      QPixmap scaledEqualizedPixmap = equalizedPixmap.scaled(ui->tabHistEqualizedImage->size(), Qt::IgnoreAspectRatio);
-      ui->tabHistEqualizedImage->setPixmap(scaledEqualizedPixmap);
-
-      vector<int> equalizedHistogram = Histograms::equalizedHistogram(grayscaleMat, histogram, scale);
-
-      // Normalize
-      Mat matNormalized = Histograms::normalizeMat(grayscaleMat, 0, 255);
-      QPixmap normalizedPixmap = HelperFunctions::convertMatToPixmap(matNormalized);
-      QPixmap scaledNormalizedPixmap = normalizedPixmap.scaled(ui->tabHistNormalizedImage->size(), Qt::IgnoreAspectRatio);
-      ui->tabHistNormalizedImage->setPixmap(scaledNormalizedPixmap);
-
-      HistoTabImage.setNormalizedImage(matNormalized);
-
-      plotHistogram(histogram, "Histogram", ui->histogramWIdget);
-      plotHistogram(cumlautiveHistogram, "Cumulative Histogram", ui->cumhistogramWIdget);
-      plotHistogram(equalizedHistogram, "EqualizedHistogram", ui->equhistogramWIdget);
-
-    }
-}
-
-// ------------------------  Hybird Tab Handlers ------------------------ //
-
-
-void MainWindow::on_uploadImage1_clicked()
-{
-  Mat Image = HelperFunctions::readImage_Mat();
-  if (Image.cols != 1 && Image.rows != 1){
-      HybridImage1.setFirstUpload(Image);
-      QPixmap pixmap = HelperFunctions::convertMatToPixmap(Image);
-      QPixmap scaledpixmap = pixmap.scaled(ui->tabMixerImage1->size(), Qt::IgnoreAspectRatio);
-      ui->tabMixerImage1->setPixmap(scaledpixmap);
-    }
-}
-
-void MainWindow::on_downloadEqualized_clicked()
-{
-  downloadImage(HistoTabImage.getFilteredImage());
-}
-
-void MainWindow::on_downloadNormalized_clicked()
-{
-  downloadImage(HistoTabImage.getNormalizedImage());
-}
-
-void MainWindow::on_mixImagesBtn_clicked()
-{
-  Mat outputImageHigh = FourierMix::apply_filter(HybridImage1.getCurrentImage(), "Ideal High Pass",d1note);
-  Mat outputImageLow = FourierMix::apply_filter(HybridImage2.getCurrentImage(), "Ideal Low Pass", d2note);
-
-  Mat mixed = FourierMix::mix_images(outputImageHigh,outputImageLow);
-
-  QPixmap pixmap = HelperFunctions::convertMatToPixmap(mixed);
-  QPixmap scaledpixmap = pixmap.scaled(ui->tabMixerImage2->size(), Qt::IgnoreAspectRatio);
-  ui->tabMixerImageMixed->setPixmap(scaledpixmap);
-}
-
-void MainWindow::on_uploadImage2_clicked()
-{
-  Mat Image = HelperFunctions::readImage_Mat();
-  if (Image.cols != 1 && Image.rows != 1){
-      HybridImage2.setFirstUpload(Image);
-      QPixmap pixmap = HelperFunctions::convertMatToPixmap(Image);
-      QPixmap scaledpixmap = pixmap.scaled(ui->tabMixerImage2->size(), Qt::IgnoreAspectRatio);
-      ui->tabMixerImage2->setPixmap(scaledpixmap);
+void MainWindow::on_btnPlus_clicked() {
+  if(index < ui->stackedWidget->count() - 1){
+      ui->stackedWidget->setCurrentIndex(++index);
+      ui->pageIndex->setText(QString::number(index + 1));
     }
 }
 
 
-void MainWindow::on_kernelSlider_valueChanged(int value)
+void MainWindow::on_btnMinus_clicked()
 {
-  int boxVal;
-  switch (value) {
-    case 1:{
-        boxVal = value;
-        break;
-      }
-    case 2: {
-        boxVal = value+1;
-        break;
-      }
-    case 3: {
-        boxVal =value+2;
-        break;
-      }
-    case 4: {
-        boxVal = value+3;
-        break;
-      }
-    case 5: {
-        boxVal = value+4;
-        break;
-      }
-    case 6: {
-        boxVal = value+5;
-        break;
-      }
+  if (index > 0){
+      ui->stackedWidget->setCurrentIndex(--index);
+      ui->pageIndex->setText(QString::number(index + 1));
     }
-  ;
-
-  filterTabImage.kenerlSize = boxVal;
-  ui->kernelValue->setText(QString::number(boxVal));
-}
-
-
-
-void MainWindow::on_sigmaSlider_valueChanged(int value)
-{
-  filterTabImage.sigma = value;
-  ui->sigmaValue->setText(QString::number(value));
-}
-
-
-void MainWindow::on_meanSlider_valueChanged(int value)
-{
-  filterTabImage.mean = value;
-  ui->meanValue->setText(QString::number(value));
-}
-
-void MainWindow::on_pic1d0_valueChanged(int value)
-{
-  d1note = value;
-  ui->d1noteVal->setText(QString::number(value));
-}
-
-
-void MainWindow::on_pic2d0_valueChanged(int value)
-{
-  d2note = value;
-  ui->d2noteVal->setText(QString::number(value));
 }
 
--- src/UI/mainwindow.h
+++ src/UI/mainwindow.h
@@ -1,106 +1,31 @@
-/******************************************************************************
- *
- * File Name: mainwindow.h
- * Description: Header file for the GUI main window of the program
- * Authors: Ibrahim Mohamed
- * Last Modified: 3 Mar 23 - 01:40
- *
- *******************************************************************************/
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <vector>
-#include <QtCharts>
 
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
-#include "src/Helpers/image.h"
 
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-using namespace std;
-
-namespace Ui {
-  class MainWindow;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
   Q_OBJECT
 
 public:
-  explicit MainWindow(QWidget *parent = 0);
+  MainWindow(QWidget *parent = nullptr);
   ~MainWindow();
-  void updateFilteredPicture(cv::Mat updatedImage);
-  void updateCurrentPicture(cv::Mat updatedImage);
-  void downloadImage(Mat image);
-  void plotHistogram(const vector<int>& data , QString ChartName, QChartView* widget);
 
 private slots:
+  void on_btnPlus_clicked();
 
-  void on_uploadBtn_clicked();
-
-  void on_averageRemoveBtn_clicked();
-
-  void on_downloadBtn_clicked();
-
-  void on_gaussianRemoveBtn_clicked();
-
-  void on_medianRemoveBtn_clicked();
-
-  void on_uniformNoiseBtn_clicked();
-
-  void on_saltNoiseBtn_clicked();
-
-  void on_gaussianNoiseBtn_clicked();
-
-  void on_sobelBtn_clicked();
-
-  void on_robertsBtn_clicked();
-
-  void on_prewittBtn_clicked();
-
-  void on_cannyBtn_clicked();
-
-  void on_lowFrequencyFilterBtn_clicked();
-
-  void on_highFrequencyFilterBtn_clicked();
-
-  void on_gaussianFrequencyFilterBtn_clicked();
-
-  void on_applyBtn_clicked();
-
-  void on_resetBtn_clicked();
-
-  void on_uploadRGB_clicked();
-
-  void on_downloadGray_clicked();
-
-  void on_uploadHist_clicked();
-
-  void on_uploadImage1_clicked();
-
-  void on_downloadEqualized_clicked();
-
-  void on_downloadNormalized_clicked();
-
-  void on_mixImagesBtn_clicked();
-
-  void on_uploadImage2_clicked();
-
-  void on_kernelSlider_valueChanged(int value);
-
-  void on_sigmaSlider_valueChanged(int value);
-
-  void on_meanSlider_valueChanged(int value);
-
-  void on_pic1d0_valueChanged(int value);
-
-  void on_pic2d0_valueChanged(int value);
+  void on_btnMinus_clicked();
 
 private:
   Ui::MainWindow *ui;
+  int index;
 };
-
 #endif // MAINWINDOW_H
