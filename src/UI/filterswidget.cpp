1e36f454fe925af2726f8987debd754bfeef02b9
Ibrahim <imimazim7@gmail.com>
2023-05-11 22:14:35 +0200

    added validations on filters widget

--- src/UI/filterswidget.cpp
+++ src/UI/filterswidget.cpp
@@ -73,7 +73,8 @@ void FiltersWidget::on_uploadBtn_clicked()
 
 void FiltersWidget::on_downloadBtn_clicked()
 {
-  HelperFunctions::downloadImage(this->filtersWidgetImage.filteredImage);
+  if(!filtersWidgetImage.currentImage.empty())
+    HelperFunctions::downloadImage(this->filtersWidgetImage.filteredImage);
 }
 
 // Add Noise
@@ -84,64 +85,82 @@ void FiltersWidget::on_uniformNoiseBtn_clicked()
 
 void FiltersWidget::on_guassianNoiseBtn_clicked()
 {
-  Mat outputImage = NoiseAddatives::GaussianNoise(this->filtersWidgetImage.currentImage,
-                                                  this->filtersWidgetImage.sigma ,
-                                                  this->filtersWidgetImage.mean);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = NoiseAddatives::GaussianNoise(this->filtersWidgetImage.currentImage,
+                                                    this->filtersWidgetImage.sigma ,
+                                                    this->filtersWidgetImage.mean);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 void FiltersWidget::on_saltNoiseBtn_clicked()
 {
-  Mat outputImage = NoiseAddatives::SaltAndPepperNoise(this->filtersWidgetImage.currentImage);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = NoiseAddatives::SaltAndPepperNoise(this->filtersWidgetImage.currentImage);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 
 // Remove Noise
 void FiltersWidget::on_avgFilterBtn_clicked()
 {
-  Mat outputImage = ImageSmoothers::applyAverageFilter(this->filtersWidgetImage.currentImage, this->filtersWidgetImage.kernelSize);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = ImageSmoothers::applyAverageFilter(this->filtersWidgetImage.currentImage, this->filtersWidgetImage.kernelSize);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 void FiltersWidget::on_guasssianFilterBtn_clicked()
 {
-  Mat outputImage = ImageSmoothers::applyGaussianFilter(this->filtersWidgetImage.currentImage,
-                                                        this->filtersWidgetImage.kernelSize,
-                                                        this->filtersWidgetImage.sigma);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = ImageSmoothers::applyGaussianFilter(this->filtersWidgetImage.currentImage,
+                                                          this->filtersWidgetImage.kernelSize,
+                                                          this->filtersWidgetImage.sigma);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 void FiltersWidget::on_medianFilterBtn_clicked()
 {
-  Mat outputImage = ImageSmoothers::applyMedianFilter(this->filtersWidgetImage.currentImage, this->filtersWidgetImage.kernelSize);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = ImageSmoothers::applyMedianFilter(this->filtersWidgetImage.currentImage, this->filtersWidgetImage.kernelSize);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 
 // Edge Detection
 void FiltersWidget::on_sobelBtn_clicked()
 {
-  Mat outputImage = EdgeDetectors::SobalEdgeDetector(this->filtersWidgetImage.currentImage);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = EdgeDetectors::SobalEdgeDetector(this->filtersWidgetImage.currentImage);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 void FiltersWidget::on_robertsBtn_clicked()
 {
-  Mat outputImage = EdgeDetectors::RobertEdgeDetector(filtersWidgetImage.currentImage);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = EdgeDetectors::RobertEdgeDetector(filtersWidgetImage.currentImage);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 void FiltersWidget::on_prewittBtn_clicked()
 {
-  Mat outputImage = EdgeDetectors::PerwittEdgeDetector(filtersWidgetImage.currentImage);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = EdgeDetectors::PerwittEdgeDetector(filtersWidgetImage.currentImage);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 void FiltersWidget::on_cannyBtn_clicked()
 {
-  Mat outputImage = EdgeDetectors::CannyEdgeDetector(filtersWidgetImage.currentImage);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = EdgeDetectors::CannyEdgeDetector(filtersWidgetImage.currentImage);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 
@@ -149,36 +168,50 @@ void FiltersWidget::on_cannyBtn_clicked()
 // Frequency Filters
 void FiltersWidget::on_lowPassBtn_clicked()
 {
-  Mat outputImage = FourierMix::apply_filter(filtersWidgetImage.currentImage, "Ideal Low Pass", this->filtersWidgetImage.dNote);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = FourierMix::apply_filter(filtersWidgetImage.currentImage,
+                                               "Ideal Low Pass",
+                                               this->filtersWidgetImage.dNote);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 void FiltersWidget::on_highPassBtn_clicked()
 {
-  Mat outputImage = FourierMix::apply_filter(this->filtersWidgetImage.currentImage, "Ideal High Pass", this->filtersWidgetImage.dNote);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = FourierMix::apply_filter(this->filtersWidgetImage.currentImage,
+                                               "Ideal High Pass",
+                                               this->filtersWidgetImage.dNote);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 void FiltersWidget::on_freqGuassianBtn_clicked()
 {
-  Mat outputImage = FourierMix::apply_filter(this->filtersWidgetImage.currentImage, "Gaussian", this->filtersWidgetImage.dNote);
-  updateFilteredPicture(outputImage);
+  if(!filtersWidgetImage.currentImage.empty()){
+    Mat outputImage = FourierMix::apply_filter(this->filtersWidgetImage.currentImage, "Gaussian", this->filtersWidgetImage.dNote);
+    updateFilteredPicture(outputImage);
+  }
 }
 
 void FiltersWidget::on_lThresholdingBtn_clicked()
 {
 
-  // Apply local thresholding with a block size of 11 and a k value of 0.2
-  Mat localThresholdedImage = THRESHOLD_H::LocalThresholdImage(this->filtersWidgetImage.currentImage, 11, 0.2);
-  HelperFunctions::viewImageOnLabel(localThresholdedImage, ui->imageFiltered);
+  if(!filtersWidgetImage.currentImage.empty()){
+    // Apply local thresholding with a block size of 11 and a k value of 0.2
+    Mat localThresholdedImage = THRESHOLD_H::LocalThresholdImage(this->filtersWidgetImage.currentImage, 11, 0.2);
+    HelperFunctions::viewImageOnLabel(localThresholdedImage, ui->imageFiltered);
+  }
 
 }
 
 void FiltersWidget::on_gThresholdingBtn_clicked()
 {
-  // Apply global thresholding with a threshold value of 128
-  Mat globalThresholdedImage = THRESHOLD_H::GlobalthresholdImage(this->filtersWidgetImage.currentImage, 128);
-  HelperFunctions::viewImageOnLabel(globalThresholdedImage, ui->imageFiltered);
+  if(!filtersWidgetImage.currentImage.empty()){
+    // Apply global thresholding with a threshold value of 128
+    Mat globalThresholdedImage = THRESHOLD_H::GlobalthresholdImage(this->filtersWidgetImage.currentImage, 128);
+    HelperFunctions::viewImageOnLabel(globalThresholdedImage, ui->imageFiltered);
+  }
 }
 
 // Sliders Handelers
