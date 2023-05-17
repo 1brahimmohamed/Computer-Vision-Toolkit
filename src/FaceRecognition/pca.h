191ee9f8498b22899b90b9a44fe2296dd6b7b30e
Mahmoud Yaser Salman <88372358+mahmoud1yaser@users.noreply.github.com>
2023-05-18 01:18:04 +0300

    Training Scheme Implemented

--- src/FaceRecognition/pca.h
+++ src/FaceRecognition/pca.h
@@ -1,22 +1,30 @@
 #ifndef PCA_H
 #define PCA_H
-
-#include <QDebug>
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-
+#include<cmath>
+#include<cstdint>
+#include <random>
 using namespace std;
 using namespace cv;
 
 class Pca
 {
 public:
-  Pca();
-
+    Pca();
 private:
-  Mat CalculateCovarianceMatrix(Mat normalizedImages);
-  Mat computeEign(Mat covarMat, Mat& eigenVals);
+  static Mat CalculateCovarianceMatrix(Mat normalizedImages);
+  static Mat computePca(Mat covariance_matrix, Mat NormalizedDataMat);
+  static Mat ComputeWeights(Mat EigenFacesMat, Mat NormalizedImag);
 };
 
 #endif // PCA_H
